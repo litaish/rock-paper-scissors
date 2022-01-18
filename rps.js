e9752e2daa8da4e12921e1c2067a9b8288d7f825
litaish <l.strode000@gmail.com>
2022-01-18 23:04:23 +0200

    Added wins counter. Based on outcome, console displays the winner.

--- rps.js
+++ rps.js
@@ -9,6 +9,10 @@ function getRandom(min, max){
     max = Math.ceil(max)
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
+// Global wins counters
+let playerWins = 0;
+let computerWins = 0;
+
 function playRound(playerSelection, computerSelection){
     // Convert player selection to lowercase
     let playerAnswer = playerSelection.toLowerCase().trim();
@@ -16,30 +20,49 @@ function playRound(playerSelection, computerSelection){
 
     // Checking if player selection matches computer selection
     if(playerAnswer == computerSelection.toLowerCase()){
+        ++playerWins;
+        ++computerWins;
         return 'It\'s a tie!'
     } else {
         switch (playerAnswer){
             case 'rock':
                 if(computerSelection == 'Paper'){
+                    ++computerWins
                     return 'You lose! Paper beats rock!'
                 }
                 else {
+                    ++playerWins;
                     return 'You win! Rock beats scissors!'
                 }
             case 'paper':
                 if(computerSelection == 'Rock'){
+                    ++playerWins;
                     return 'You win! Paper beats rock!'
                 } else {
+                    ++computerWins
                     return 'You lose! Scissors beat paper!'
                 }
             case 'scissors':
                 if(computerSelection == 'Rock'){
+                    ++computerWins
                     return 'You lose! Rock beats scissors!'
                 } else {
+                    ++playerWins;
                     return 'You win! Scissors beat paper!'
                 }
             default: 
                 return 'No such option! Choose between rock, paper and scissors.'
+        }
+    }
+}
+function checkWinsAndLosses(){
+    if(playerWins == computerWins){
+        return 'Game is a tie!'
+    } else {
+        if(playerWins > computerWins) {
+            return 'You are the winner!'
+        } else {
+            return 'Sorry, you lose.'
         }
     }
 }
@@ -51,6 +74,6 @@ function game(){
         let roundResult = playRound(playerSelection, computerSelection);
         console.log(roundResult)
     }
-    
 }
 game()
+console.log(checkWinsAndLosses())
