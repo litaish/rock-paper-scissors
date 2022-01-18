904b66a44ce9a33672561aa4c0a35f13e722edbd
litaish <l.strode000@gmail.com>
2022-01-18 22:35:58 +0200

    Finished main game mechanic - console returns correct results for every option

--- rps.js
+++ rps.js
@@ -10,9 +10,47 @@ function getRandom(min, max){
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 function playRound(playerSelection, computerSelection){
+    // Convert player selection to lowercase
+    let playerAnswer = playerSelection.toLowerCase().trim();
+    let tieMessage = 'It\'s a tie!'
 
+    // Checking if player selection matches computer selection
+    if(playerAnswer == computerSelection.toLowerCase()){
+        return 'It\'s a tie!'
+    } else {
+        switch (playerAnswer){
+            case 'rock':
+                if(computerSelection == 'Paper'){
+                    return 'You lose! Paper beats rock!'
+                }
+                else {
+                    return 'You win! Rock beats scissors!'
+                }
+            case 'paper':
+                if(computerSelection == 'Rock'){
+                    return 'You win! Paper beats rock!'
+                } else {
+                    return 'You lose! Scissors beat paper!'
+                }
+            case 'scissors':
+                if(computerSelection == 'Rock'){
+                    return 'You lose! Rock beats scissors!'
+                } else {
+                    return 'You win! Scissors beat paper!'
+                }
+            default: 
+                return 'No such option! Choose between rock, paper and scissors.'
+        }
+    }
 }
 // A game of 5 rounds
 function game(){
+    for (let i = 0; i < 5; i++){
+        let computerSelection = computerPlay();
+        let playerSelection = prompt('Please enter your choice (rock, paper or scissors): ')
+        let roundResult = playRound(playerSelection, computerSelection);
+        console.log(roundResult)
+    }
     
 }
+game()
